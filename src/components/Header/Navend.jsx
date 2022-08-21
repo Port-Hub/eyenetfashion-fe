0118c6333513c6e119df2b849b200e16af3c4c95
RahulRST <srahuljayaraj@gmail.com>
2022-08-21 16:28:27 +0530

    Ham menu typo exists

--- src/components/Header/Navend.jsx
+++ src/components/Header/Navend.jsx
@@ -4,7 +4,7 @@ class Navend extends Component {
     render() {
         return (
             <div className="navbar-end">
-                <a className="btn glass btn-outline btn-accent ">Contact Us</a>
+                <a className="btn btn-sm sm:btn-sm lg:btn-md glass btn-outline btn-accent">Contact Us</a>
             </div>
         )
     }
--- src/components/Header/Navstart.jsx
+++ src/components/Header/Navstart.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import Navlist from "./Navlist.jsx";
 
 class Navstart extends Component {
@@ -6,8 +6,13 @@ class Navstart extends Component {
         return (
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex="0" className="btn btn-ghost xl:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <label className="btn btn-ghost swap swap-rotate xl:hidden">
+                        <input type="checkbox"/>
+  
+                        <svg className="swap-off" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                      
+                        <svg className="swap-on" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+  
                     </label>
                     <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <Navlist />
