866fefaf3c686d8659ca377166f4f5ad6bd3bced
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-08-23 21:08:08 +0530

    theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,20 +10,20 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["luxury",
-      {
-        proDark: {
-          "primary": "#eda8c5",
-          "secondary": "#8056c9",
-          "accent": "#e2f49a",
-          "neutral": "#22282A",
-          "base-100": "#283143",
-          "info": "#2AA5D5",
-          "success": "#21B05F",
-          "warning": "#E09E10",
-          "error": "#E61E32",
-        },
-      },
+    themes: ["business"
+      // {
+      //   proDark: {
+      //     "primary": "#eda8c5",
+      //     "secondary": "#8056c9",
+      //     "accent": "#e2f49a",
+      //     "neutral": "#22282A",
+      //     "base-100": "#283143",
+      //     "info": "#2AA5D5",
+      //     "success": "#21B05F",
+      //     "warning": "#E09E10",
+      //     "error": "#E61E32",
+      //   },
+      // },
     ],
   },
 }
