ac7e9b31b9fa23f2686b1bd37e761635171a3b36
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-08-23 22:59:45 +0530

    photography page

--- src/components/Pages/courses/photography.jsx
+++ src/components/Pages/courses/photography.jsx
@@ -1,7 +1,24 @@
 const Photography = () => {
-    return (
-        <h1>Photography Course</h1>
-    )
-}
+  return (
+    <div class="flex flex-row" >
+      <div class="basis-3/4">
+        <h1>DIGITAL SLR COURSE</h1>
+        <h2>Introduction to DSLR Cameras</h2>
+            <ul>
+            <li>Learning about Camera menus and its functions</li> 
+            <li>Understanding Various file formats like Raw, Tiff, Jpeg etc</li>
+          <li>Mega pixel and resolution Full frame sensors and cropped frame sensors ,Crop Factor</li>
+          </ul>
+          <h2>Exposure</h2>
+            <ul>
+            <li>Understanding Shutter speed , Aperture, ISO relationships for creative images</li> 
+            <li>Understanding Various file formats like Raw, Tiff, Jpeg etc</li>
+          </ul>
+
+      </div>
+      <div class="basis-1/4">image here</div>
+    </div>
+  );
+};
 
 export default Photography;
