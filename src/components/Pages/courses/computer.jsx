e65eb84a8b5d5ca6efc6305c175c8c223dfe2cb3
RahulRST <srahuljayaraj@gmail.com>
2022-08-25 00:10:51 +0530

    computer done

--- src/components/Pages/courses/computer.jsx
+++ src/components/Pages/courses/computer.jsx
@@ -1,6 +1,193 @@
 const Computer = () => {
     return (
-        <h1>Computer Education Course</h1>
+        <>
+            <h2>Computer Education</h2>
+                <div id="gallery">
+                    <b><span>Computer</span> Basics & Applications </b>
+                    <div>
+                        <div>	
+                            <h4>Computer Basics & Applications</h4>
+                            <span>Computer Basics -- Duration 1 Month</span>
+                            <ul>
+                                <li> Windows </li>
+                                <li>  Fingering Practice</li>
+                                <li>  MS-Word</li>
+                                <li> MS-Power point Basic Internet</li>
+                            </ul>
+                            <span>Computer Applications -- Duration  2 Months </span>
+                            <ul>
+                                <li> Windows</li>
+                                <li> MS-Word, MS-Excel, MS-Powerpoint  </li>
+                                <li> Applications of Internet and usage</li>
+                                <li> Scanning file converting  and Printing Technologies.  </li>
+                            </ul>
+                            <span>Computer Programming -- Duration 2 Months  </span>
+                            <ul>
+                                <li>  C, C++ </li>
+                            </ul>
+                            <span>Computer Application &amp; Programming -- Duration 6 Months </span>
+                            <ul>
+                                <li>Computer concepts</li>
+                                <li> Windows</li>
+                                <li> Networking  principles</li>
+                                <li> Microsoft  office</li>
+                                <li> C, C++ Programming</li>
+                                <li> Visual Basic</li>
+                                <li> SQL/Oracle</li>
+                                <li> Internet</li>
+                            </ul>
+                            <span>Client Server Programming -- Duration 3 Months</span>
+                            <ul>
+                                <li>VB, MS-access/ Oracle / SQL</li>
+                            </ul>
+                        </div>
+                    </div>
+                <b><span>Web</span> Designing</b>
+                <div>	
+                    <div>
+                        <h4>Web Designing -- Duration 3 Months</h4>
+                        <span>Design ideas:</span>
+                        <ul>
+                            <li>Computer Basics</li>
+                            <li> MS-front Page</li>
+                            <li> Dreamweaver</li>
+                            <li> HTML</li>
+                            <li> Gif animation</li>
+                            <li> Photoshop</li>
+                            <li> Form submission using PHP</li>
+                        </ul>
+                        <span>Web Designing with Flash -- Duration 4 Months</span>
+                        <ul>
+                            <li> 	Flash</li>
+                        </ul>
+                        <span>Web Programming -- Duration 1.5 Months</span>
+                        <ul>
+                            <li> HTML</li>
+                            <li> CSS</li>
+                            <li> Java Script</li>
+                        </ul>
+                        <span>Web Programming -- Duration 2 Months</span>
+                        <ul>
+                            <li>ASP with My SQL/ SQL/ MS-Access</li>
+                        </ul>
+                        <span>Web Programming -- Duration 2 Months</span>
+                        <ul>
+                            <li>PHP with My SQL/ MS-Access</li>
+                        </ul>
+                        <span>Web Programming -- Duration 2 Months</span>
+                        <ul>
+                            <li>JSP with My SQL/ Oracle/ Postgre SQL</li>
+                        </ul>
+                    </div>
+                </div>
+                <b><span>Short Term</span> Courses</b>
+                <div>	
+                    <div>
+                        <h4>Short Term Courses </h4>
+                        <span>Office Automation -- Duration 15 Days</span>
+                        <ul>
+                            <li>MS-Word</li>
+                            <li> MS-Excel</li>
+                            <li> MS-Powerpoint</li>
+                        </ul>
+                        <span>Internet Training -- Duration 10 Days</span>
+                        <ul>
+                            <li> Creating an E-mail ID</li>
+                            <li> Receiving & Sending Emails</li>
+                            <li> Audio & Video chatting</li>
+                            <li>  Surfing information</li>
+                            <li> Uploading and downloading of Photos,  Audios, Videos etc.  </li>
+                        </ul>
+                        <h2>Animation & Graphics</h2>
+                        <span>Adobe Premiere Pro -- Duration 1 Month</span><br />
+                        <ul>
+                            <li>Adobe premier</li>
+                            <li> Sound forge</li>
+                            <li> Morphing, Combustion</li>
+                        </ul>
+                        <span>Crazy Talk Animator -- Duration 2 Weeks</span><br />
+                        <ul>
+                            <li>Adobe premier</li>
+                            <li> Sound forge</li>
+                            <li> Morphing, Combustion</li>
+                            <li> Combustion</li>
+                        </ul>
+                    </div>
+                </div>
+                <b><span>Animation</span> & Graphics</b>
+                <div>
+                    <div>
+                        <h4>Animation & Graphics</h4>
+                        <div>
+                            <span>Image Editing &amp; 2D Drawing -- Duration 2 Months</span><br />
+                            <ul>
+                                <li>Adobe Photoshop</li>
+                                <li> Adobe Image ready</li>
+                                <li> Corel draw</li>
+                                <li> Gif Animation</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <span>Video Production -- Duration 2 Months</span>
+                            <ul>
+                                <li>Adobe premier</li>
+                                <li> Sound forge</li>
+                                <li> Morphing</li>
+                                <li> Combustion</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <span>Flash -- Duration 1 Month</span>
+                            <ul>
+                                <li>2D drawing</li>
+                                <li> Timeline effects,  Key frame Animation</li>
+                                <li> Basic Action Scripts</li>
+                                <li> Movie clips,  etc.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <span>2D Animation -- Duration 2 Months</span><br />
+                            <ul>
+                                <li>Flash</li>
+                                <li> Swish Max </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <strong>Project :</strong>
+                            <p>
+                                2D character Animation, Text animation,  
+                                Special Effects, Animation using  script
+                            </p>
+                        </div>
+                        <span>Adobe Photoshop -- Duration 1 Month</span>
+                        <ul>
+                            <li>Animation, Image editing</li>
+                            <li> Filters, Special effects</li>
+                            <li> Color manipulation etc.</li>
+                        </ul>
+                        <strong>Project :</strong> <br />
+                        <p>
+                            Photo manipulation, Blending images, Banner designing, 
+                            Designing of ID card, Greeting card, Visiting Card, 
+                            and other  Creative works.
+                        </p>
+                        <span>Coreldraw -- Duration 1 Month</span><br />
+                        <ul>
+                            <li>2D Design & Line Drawing</li>
+                            <li> Logo & Emblem creations</li>
+                            <li> Filters, Special effects</li>
+                        </ul>
+                        <strong>Project :</strong>
+                        <p>
+                            Designing of Greeting card, Visiting Card,  ID card and other  
+                            Creative works. Broachers and Pham lets designing, 
+                            Letter head designing etc.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+
     )
 }
 
