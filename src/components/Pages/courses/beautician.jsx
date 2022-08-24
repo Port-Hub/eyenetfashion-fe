a324b0372cfcb62e214612f47d44f17df9837407
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-08-24 22:32:58 +0530

    beautician page half(error)

--- src/components/Pages/courses/beautician.jsx
+++ src/components/Pages/courses/beautician.jsx
@@ -1,6 +1,32 @@
+import Pg1img1 from "../../assets/beautician/Beautician_1.jpg";
+import Pg1img2 from"../../assets/beautician/Parlour_Infra_1.jpg";
+import Pg1img3 from"../../assets/beautician/Parlour_Infra.jpg";
+import Pg1img4 from"../../assets/beautician/Parlour_Infra_2.jpg";
+import Pg1img5 from"../../assets/beautician/Parlour_Infra_3.jpg";
 const Beautician = () => {
     return (
-        <h1>Beautician Course</h1>
+        <div>
+            <h4>Best Training Institute for Beautician Courses</h4>
+                <img src={Pg1img1} />
+                <img src={Pg1img2}/>
+                <img src={Pg1img3} />
+                <img src={Pg1img4}/>
+                <img src={Pg1img5}/>
+                <br />
+                A thing of Beauty is a Joy Forever. True to its sense, looking at beautiful things always gives pleasure or joy to everyone. 
+                Every person in this world is attracted to beauty and want to look beautiful. There has been an increased awareness about beauty and grooming in the past years as more and more people have started to go to Beauty Parlours & Spas to enhance their appearance<br />
+                <br />
+                The Art of Beauty & Cosmetology has helped many ordinary women to become beautiful and thereby boosting their confidence and career. In recent years there has been an increase in the number of beauty parlour all over the country. 
+                This created a demand for beauticians who have completed any Beauty parlour courses, Cosmetology courses, Beauty Therapist courses, or Hair and Beauty courses from a reputed Institute for Beautician courses<br />
+                <br />
+                There are many parlours offering Beautician courses in South India. But they are more of practical in nature and are not professional in training. 
+                NCFT Heights is the only institute offering Beautician courses in Tamil Nadu, karnataka & kerala at affordable fees for all segments of people. 
+                NCFT Heights is the best Training Centre for Beautician Courses with well trained faculty and up to date study materials.
+                 At NCFT Heights we offer different beauty related courses like Certificate Courses for Bridal Make up , Cosmetology Courses, Beauty care Courses, Beauty Short term Courses , Beauty Therapist Courses , and Hair & Beauty Courses . 
+                 By completing any of these courses one can either take up a job with a reputed beauty salon and spa or Start their own Beautician institute and provide employment to others. 
+                Being a Career in Beautician is a respectable profession with a very high earnings.
+                <br />
+        </div>
     )
 }
 
