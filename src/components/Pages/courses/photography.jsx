d438899d2e5395666677db385d2efa6e8aae89ca
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-08-29 21:11:44 +0530

    icons for photography page

--- src/components/Pages/courses/photography.jsx
+++ src/components/Pages/courses/photography.jsx
@@ -78,7 +78,7 @@ const mapChild = (param) => {
 //         </p>
 //       </div>
 //     </div>
-//   </div> 
+//   </div>
 //   ));
 // }
 
@@ -90,62 +90,155 @@ const Photography = () => {
       <h1 className="text-center text-4xl font-semibold py-6">
         DIGITAL SLR COURSE
       </h1>
-      <div
-        className="section pt-20 pb-8 md:pt-16 md:pb-0"
-      >
+      <div className="section pt-20 pb-8 md:pt-16 md:pb-0">
         <div className="container xl:max-w-15xl mx-auto px-4">
-        {/* <!-- row --> */}
-        <div className="flex flex-wrap flex-row -mx-4 text-center">
-            <div className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp " data-wow-duration="1s" styles="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
-                {/* <!-- service block --> */}
-                <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-                    <div className="inline-block text-gray-900 mb-4">
-                    </div>
-                    <h3 className="text-lg leading-normal mb-2 font-semibold text-black">{photoArray[0].title}</h3>
-                    <p className="text-gray-500">{mapChild(photoArray[0].points)}</p>
+          {/* <!-- row --> */}
+          <div className="flex flex-wrap flex-row -mx-4 text-center">
+            <div
+              className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp "
+              data-wow-duration="1s"
+              styles="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;"
+            >
+              {/* <!-- service block --> */}
+              <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+                <div className="inline-block text-gray-900 mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M12 9a3.75 3.75 0 100 7.5A3.75 3.75 0 0012 9z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M9.344 3.071a49.52 49.52 0 015.312 0c.967.052 1.83.585 2.332 1.39l.821 1.317c.24.383.645.643 1.11.71.386.054.77.113 1.152.177 1.432.239 2.429 1.493 2.429 2.909V18a3 3 0 01-3 3h-15a3 3 0 01-3-3V9.574c0-1.416.997-2.67 2.429-2.909.382-.064.766-.123 1.151-.178a1.56 1.56 0 001.11-.71l.822-1.315a2.942 2.942 0 012.332-1.39zM6.75 12.75a5.25 5.25 0 1110.5 0 5.25 5.25 0 01-10.5 0zm12-1.5a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </div>
-                {/* <!-- end service block --> */}
+                <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+                  {photoArray[0].title}
+                </h3>
+                <p className="text-gray-500">
+                  {mapChild(photoArray[0].points)}
+                </p>
+              </div>
+              {/* <!-- end service block --> */}
             </div>
-            <div className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s" styles="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;">
-                {/* <!-- service block --> */}
-                <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-                    <div className="inline-block text-gray-900 mb-4">
-                    </div>
-                    <h3 className="text-lg leading-normal mb-2 font-semibold text-black">{photoArray[1].title}</h3>
-                    <p className="text-gray-500">{mapChild(photoArray[1].points)}</p>
+            <div
+              className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+              data-wow-duration="1s"
+              data-wow-delay=".1s"
+              styles="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;"
+            >
+              {/* <!-- service block --> */}
+              <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+                <div className="inline-block text-gray-900 mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                  </svg>
                 </div>
-                {/* <!-- end service block --> */}
+                <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+                  {photoArray[1].title}
+                </h3>
+                <p className="text-gray-500">
+                  {mapChild(photoArray[1].points)}
+                </p>
+              </div>
+              {/* <!-- end service block --> */}
             </div>
-            <div className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s" styles="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;">
-                {/* <!-- service block --> */}
-                <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-                    <div className="inline-block text-gray-900 mb-4">
-                    </div>
-                    <h3 className="text-lg leading-normal mb-2 font-semibold text-black">{photoArray[2].title}</h3>
-                    <p className="text-gray-500">{mapChild(photoArray[2].points)}</p>
+            <div
+              className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+              data-wow-duration="1s"
+              data-wow-delay=".3s"
+              styles="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;"
+            >
+              {/* <!-- service block --> */}
+              <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+                <div className="inline-block text-gray-900 mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path d="M9.827 21.763L14.31 14l3.532 6.117A9.955 9.955 0 0112 22c-.746 0-1.473-.082-2.173-.237zM7.89 21.12A10.028 10.028 0 012.458 15h8.965L7.89 21.119zM2.05 13a9.964 9.964 0 012.583-7.761L9.112 13H2.05zm4.109-9.117A9.955 9.955 0 0112 2c.746 0 1.473.082 2.173.237L9.69 10 6.159 3.883zM16.11 2.88A10.028 10.028 0 0121.542 9h-8.965l3.533-6.119zM21.95 11a9.964 9.964 0 01-2.583 7.761L14.888 11h7.064z"></path>
+                  </svg>
                 </div>
-                {/* <!-- end service block --> */}
+                <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+                  {photoArray[2].title}
+                </h3>
+                <p className="text-gray-500">
+                  {mapChild(photoArray[2].points)}
+                </p>
+              </div>
+              {/* <!-- end service block --> */}
             </div>
-            <div className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" styles="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
-                {/* <!-- service block --> */}
-                <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-                    <div className="inline-block text-gray-900 mb-4">
-                    </div>
-                    <h3 className="text-lg leading-normal mb-2 font-semibold text-black">{photoArray[3].title}</h3>
-                    <p className="text-gray-500">{mapChild(photoArray[3].points)}</p>
+            <div
+              className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+              data-wow-duration="1s"
+              styles="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;"
+            >
+              {/* <!-- service block --> */}
+              <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+                <div className="inline-block text-gray-900 mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M12 .75a8.25 8.25 0 00-4.135 15.39c.686.398 1.115 1.008 1.134 1.623a.75.75 0 00.577.706c.352.083.71.148 1.074.195.323.041.6-.218.6-.544v-4.661a6.714 6.714 0 01-.937-.171.75.75 0 11.374-1.453 5.261 5.261 0 002.626 0 .75.75 0 11.374 1.452 6.712 6.712 0 01-.937.172v4.66c0 .327.277.586.6.545.364-.047.722-.112 1.074-.195a.75.75 0 00.577-.706c.02-.615.448-1.225 1.134-1.623A8.25 8.25 0 0012 .75z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M9.013 19.9a.75.75 0 01.877-.597 11.319 11.319 0 004.22 0 .75.75 0 11.28 1.473 12.819 12.819 0 01-4.78 0 .75.75 0 01-.597-.876zM9.754 22.344a.75.75 0 01.824-.668 13.682 13.682 0 002.844 0 .75.75 0 11.156 1.492 15.156 15.156 0 01-3.156 0 .75.75 0 01-.668-.824z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </div>
-                {/* <!-- end service block --> */}
+                <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+                  {photoArray[3].title}
+                </h3>
+                <p className="text-gray-500">
+                  {mapChild(photoArray[3].points)}
+                </p>
+              </div>
+              {/* <!-- end service block --> */}
             </div>
-            <div className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s" styles="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;">
-                {/* <!-- service block --> */}
-                <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
-                    <h3 className="text-lg leading-normal mb-2 font-semibold text-black">{photoArray[4].title}</h3>
-                    <p className="text-gray-500">{mapChild(photoArray[4].points)}</p>
+            <div
+              className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp"
+              data-wow-duration="1s"
+              data-wow-delay=".1s"
+              styles="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeInUp;"
+            >
+              {/* <!-- service block --> */}
+              <div className="py-8 px-12 mb-12 bg-gray-50 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
+                <div className="inline-block text-gray-900 mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+                  </svg>
                 </div>
-                {/* <!-- end service block --> */}
+                <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+                  {photoArray[4].title}
+                </h3>
+                <p className="text-gray-500">
+                  {mapChild(photoArray[4].points)}
+                </p>
+              </div>
+              {/* <!-- end service block --> */}
             </div>
-        </div>
-      
+          </div>
         </div>
       </div>
     </>
