12eb47dee37a0caf1ea0a4bc8d22a43b50708e22
RahulRST <srahuljayaraj@gmail.com>
2022-08-31 09:32:03 +0530

    Dark Switch Done

--- src/components/Header/Buttons.jsx
+++ src/components/Header/Buttons.jsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
 
-const Buttons = () => {
-    const lbtn = <button id="light" data-set-theme="cmyk" data-act-class="ACTIVECLASS"></button>;
-    const dbtn = <button id="dark" data-set-theme="forest" data-act-class="ACTIVECLASS"></button>;
+const Buttons = (props) => {
+    const lbtn = <button id="light" ref={props.light} data-set-theme="cmyk" data-act-class="ACTIVECLASS"></button>;
+    const dbtn = <button id="dark" ref={props.dark} data-set-theme="forest" data-act-class="ACTIVECLASS"></button>;
     useEffect(() => {
         themeChange(false)
     }, [])
     return (
         <>
-        {lbtn}
-        {dbtn}
+            {lbtn}
+            {dbtn}
         </>
     )
 }
--- src/components/Header/Navend.jsx
+++ src/components/Header/Navend.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
+import { useState, createRef } from "react";
 import { Link } from "react-router-dom";
 import Buttons from "./Buttons";
 
 const Navend = () => {
     const [check, setCheck] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
-    const lbutton = document.getElementById('light');
-    const dbutton = document.getElementById("dark");
+    const lbutton = createRef();
+    const dbutton = createRef();
 
     const themeSwitch = () => {
         setCheck(!check);
         if(check){
-            lbutton.click();
+            lbutton.current.click();
         }
         else{
-            dbutton.click();
+            dbutton.current.click();
         }
     }
     return (
         <div className="navbar-end space-x-5">
-            <Buttons />
+            <Buttons light={lbutton} dark={dbutton} />
             <input type="checkbox" checked={check} onChange={themeSwitch} className="toggle toggle-sm toggle-primary px-4" />
             <Link className="btn btn-sm btn-outline btn-primary" to="/contact">Contact</Link>
         </div>
