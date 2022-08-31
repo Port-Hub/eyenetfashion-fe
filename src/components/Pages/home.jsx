83862554364cf5e0ce976a25d73a1c73d5a4aa5b
RahulRST <srahuljayaraj@gmail.com>
2022-08-31 20:37:05 +0530

    Basic 360

--- src/components/Pages/home.jsx
+++ src/components/Pages/home.jsx
@@ -1,6 +1,15 @@
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+
 const Home = () => {
     return(
-        <h1>Home</h1>
+        <Canvas>
+            <Suspense fallback={null} >
+                <OrbitControls />
+                <Environment preset="dawn" background />
+            </Suspense>
+        </Canvas>
     )
 }
 
