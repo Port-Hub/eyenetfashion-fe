cef361c79943f3443108c7fd234221699b3bb825
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-08-31 15:05:43 +0530

    icons spoken english

--- src/components/Pages/courses/spokeneng.jsx
+++ src/components/Pages/courses/spokeneng.jsx
@@ -1,127 +1,224 @@
 const spokenArray = [
   {
-    title:"Basic English",
-    points:[
+    title: "Basic English",
+    points: [
       "Tense",
       "Question Making",
       "Parts of Speech",
-      "Active and Passive Voice"
-    ]
+      "Active and Passive Voice",
+    ],
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={40}
+        height={40}
+        viewBox="0,0,54,54"
+        style={{
+          enableBackground: "new 0 0 54.483 54.483",
+        }}
+        xmlSpace="preserve"
+      >
+        <path d="M29.77 35.434h16.077v2.336H29.77v-2.336zm0-3.234h16.077v-2.336H29.77V32.2zm8.032-8.035H29.77V26.5h8.032v-2.335zm0-6.266H29.77v2.336h8.032v-2.336zM29.77 43.308h16.077v-2.336H29.77v2.336zm24.713-33.156v39.005H26.561v.055l-.236-.055H0V10.152h2.327v-4.88l22.134 4.88h30.022zm-2.029 18.125h-2.747v2.755h2.747v-2.755zM4.665 12.952v29.036l19.561 4.317V12.49l-10.609-2.34-8.952-1.975v4.777zm8.953 33.406L3.497 44.123v2.235h10.121zm12.943 0h21.121V12.954h-2.689V26.79l-2.835-2.628-2.829 2.628V12.954H26.563v33.404h-.002zm-4.202-29.829-10.736-3.575-3.765-1.253-.419 1.253-.325.962 14.5 4.828.745-2.215zm0 6.261-14.5-4.828-.739 2.217 14.501 4.828.738-2.217zm0 6.259-14.5-4.829-.739 2.217 14.501 4.829.738-2.217zm0 6.262-14.5-4.824-.739 2.215 14.501 4.824.738-2.215zm0 5.543-14.5-4.831-.74 2.217 14.501 4.829.739-2.215z" />
+      </svg>
+    ),
   },
   {
-    title:"General English",
-    points:[
+    title: "General English",
+    points: [
       "Funny Words",
       "Usage",
       "Vocabulary Development",
       "Pronunciation Practice",
-      "Reading and Listening"
-    ]
+      "Reading and Listening",
+    ],
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={40}
+        height={40}
+        style={{
+          enableBackground: "new 0 0 54.483 54.483",
+        }}
+        viewBox="0 0 256 256"
+        xmlSpace="preserve"
+      >
+        <path d="M88.7 47.3c0-22 17.8-39.9 39.9-39.9 22 0 39.9 17.8 39.9 39.9 0 22-17.8 40-39.9 40-22.1-.1-39.9-18-39.9-40zM256 205.7c0 12.6-10.3 22.8-22.8 22.8-3 0-5.9-.6-8.4-1.7l-35.7-9.9V252H65v-35.1l-35 9.9c-2.5 1-5 1.6-8 1.6-12.6 0-22.7-10.2-22.7-22.8 0-4.7 1.4-9.1 4-12.7l58.9-80.7S69.9 98 88.1 97h79.3c18.2 1 25.9 15.2 25.9 15.2l58.9 80.8c2.4 3.7 3.8 8 3.8 12.7zm-67-81.4-62 20.1-62-20.1v47.4l4.7-1.5s-.1 0 0 0l-.7-.2v-6.5l54 17v6.5l-25.1-8c6.8 11 3.3 27.6-15.3 33.1l-1 .2 46.1 15.3 46-15.3-1-.2c-2.3-.7-4.3-1.5-6.1-2.5L132 220.7v-6.5l28.5-9.1c-1.3-1.3-2.5-2.7-3.3-4.2L132 209v-6.5l22.8-7.3c-.4-1.7-.7-3.4-.7-5.1l-22.2 7.1v-6.5l23.4-7.5c.8-2.3 1.9-4.4 3.4-6.4l-26.7 8.6V179l53-17v6.5l-3 .9c.9.2 1.9.4 2.8.7l4.1 1.5v-47.3zM69 146.5l54 17V157l-54-17v6.5zm0 11.7 54 17v-6.5l-54-17v6.5zm116-22-53 17v20.5l53-17v-20.5z" />
+      </svg>
+    ),
   },
   {
-    title:"Higher Grammar",
-    points:[
+    title: "Higher Grammar",
+    points: [
       "Phrasal verbs",
       "Phrase",
       "Clauses",
-      "Direct and Indirect Speech"
-    ]
+      "Direct and Indirect Speech",
+    ],
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        // className="w-6 h-6"
+        width={40}
+        height={40}
+      >
+        <path d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z" />
+        <path d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z" />
+        <path d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z" />
+      </svg>
+    ),
   },
   {
-    title:"Personality Development",
-    points:[
+    title: "Personality Development",
+    points: [
       "Common - Sense Development",
       "Interpersonal Relationship Skills",
       "Public Speaking",
       "Negotiation Ability",
       "Hotel Manners",
-      "EQ Development"
-    ]
+      "EQ Development",
+    ],
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={40}
+        height={40}
+        viewBox="0 0 488.62 488.62"
+        style={{
+          enableBackground: "new 0 0 488.62 488.62",
+        }}
+        xmlSpace="preserve"
+      >
+        <path d="M298.442 339.4c-15.9 5.2-32.4 7.8-49.2 7.8-42 0-81.5-16.4-111.3-46.1-29.7-29.7-46.1-69.2-46.1-111.3 0-42 16.4-81.6 46.1-111.3 29.8-29.6 69.3-46 111.3-46 20.4 0 40.3 3.9 59.2 11.5 19.5 7.9 37.1 19.6 52.1 34.6 27 27 43.2 62.7 45.7 100.7 1.9 29-4.2 57.7-17.3 83.1l21.5 21.5c17.4-28.3 27.4-61.7 27.4-97.3 0-103.1-83.5-186.6-186.6-186.6-28.5 0-55.6 6.4-79.7 17.9-63.7 29.6-107.9 94.1-107.9 169v31l-38 72.6c-9.6 18.3-.5 33.3 20.2 33.3h17.9v69c0 21.6 17.6 39.2 39.2 39.2l53.8-8 .1 53.8v.4c.3 6.9 6.1 11.5 13.3 10.2l194-34.3c7.3-1.3 13.3-8.4 13.3-15.9V385.1l-55.5-55.5c-7.6 3.9-15.5 7.2-23.5 9.8z" />
+        <path d="m459.142 362.5-90.9-90.9-4.9 4.9-5-5c40.1-53.4 35.8-129.5-12.7-178-53.2-53.2-139.5-53.2-192.7 0s-53.2 139.5 0 192.7c48.6 48.6 124.7 52.8 178 12.7l5 5-4.9 4.9 90.9 90.9c10.3 10.3 27 10.3 37.3 0 10.1-10.2 10.1-26.9-.1-37.2zm-135.9-98.6c-40.9 40.9-107.2 40.9-148.1 0-40.9-40.9-40.9-107.2 0-148.1s107.2-40.9 148.1 0c40.9 41 40.9 107.3 0 148.1z" />
+        <circle cx={249.242} cy={189.9} r={17.1} />
+        <circle cx={302.042} cy={189.9} r={17.1} />
+        <circle cx={196.442} cy={189.9} r={17.1} />
+      </svg>
+    ),
   },
   {
-    title:"Practicals",
-    points:[
+    title: "Practicals",
+    points: [
       "Mock Interview",
       "Mono-Acting",
       "Dumb-Charades",
       "Role-Play",
-      "Reading and Writing Exercises"
-    ]
+      "Reading and Writing Exercises",
+    ],
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={40}
+        height={40}
+        style={{
+          enableBackground: "new 0 0 943.118 943.118",
+        }}
+        xmlSpace="preserve"
+        viewBox="0 0 943 943"
+      >
+        <path d="M54.182 670.915v189.128c0 11.047 8.955 20 20 20h362.347a51.681 51.681 0 0 1-4.899-22.006V670.915H54.182zM30 639.904H431.628v-41.652c0-.229.007-.456.009-.685.107-15.218 3.8-29.6 10.277-42.337a95.486 95.486 0 0 1 9.873-15.506 96.144 96.144 0 0 1 15.245-15.507c16.361-13.248 37.182-21.197 59.827-21.197h22.555v-43.313c0-32.846-26.627-59.473-59.473-59.473h-53.809a22.628 22.628 0 0 0-22.029 17.455L389.09 524.218l-3.642 15.507-2.578 10.977a53.74 53.74 0 0 1-1.271 4.528h-16.584a118.433 118.433 0 0 0-1.577-15.506c-.148-.892-.306-1.779-.476-2.666l-3.326-12.841-19.571-75.542 15.62-34.473c2.965-6.545-1.82-13.968-9.006-13.968h-33.525c-7.186 0-11.972 7.423-9.006 13.968l15.62 34.473-20.313 75.542-3.086 11.478a120.142 120.142 0 0 0-.728 4.029 118.692 118.692 0 0 0-1.527 15.506h-15.88a55.402 55.402 0 0 1-1.271-4.528l-2.578-10.977-3.641-15.508-25.013-106.525a22.626 22.626 0 0 0-22.029-17.455h-53.808c-32.846 0-59.473 26.627-59.473 59.473v95.525H30c-16.568 0-30 13.431-30 30v24.674c0 16.565 13.432 29.995 30 29.995zM329.919 368.094c73.717 0 133.477-59.76 133.477-133.477V92.744c0-18.391-16.561-32.347-34.686-29.233-39.276 6.747-128.839 24.62-184.565 35.864-27.752 5.599-47.704 29.986-47.704 58.297v76.946c.001 73.717 59.761 133.476 133.478 133.476z" />
+        <path d="M526.859 533.021a64.967 64.967 0 0 0-28.812 6.703 65.426 65.426 0 0 0-20.201 15.506c-9.95 11.325-16.036 26.118-16.204 42.337-.002.229-.017.455-.017.685v259.785c0 12.154 9.853 22.006 22.005 22.006h437.482c12.153 0 22.006-9.852 22.006-22.006V598.252c0-31.565-22.422-57.893-52.209-63.928a65.554 65.554 0 0 0-13.021-1.303H526.859zM702.375 497.769c80.854 0 146.4-65.546 146.4-146.4v-84.396c0-31.052-21.886-57.8-52.322-63.941-61.123-12.332-159.355-31.935-202.434-39.336a32.901 32.901 0 0 0-5.577-.478c-17.574 0-32.468 14.276-32.468 32.542v155.609c.001 80.854 65.546 146.4 146.401 146.4z" />
+      </svg>
+    ),
   },
   {
-    title:"Out-Door Activities",
-    points:[
+    title: "Out-Door Activities",
+    points: [
       "Jungle Camp",
       "English Style Dining",
       "Archaeological Site Visit",
       "Bird Watching Trips",
-      "Culture-Gazing"
-    ]
+      "Culture-Gazing",
+    ],
+    svg: (
+      <svg
+         width={40}
+        height={40}
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        viewBox="0 0 512 512"
+      >
+        <path d="M149.9 27.2 34.25 56.74v76.76L157.8 93.85l46.7-44.67-54.6-21.98zm132.8 57c-7.4.18-10.1 1.88.9 7.13C346.9 121.6 441.7 206.8 391.3 216.9 232.2 249 130.4 292.3 48.51 390.8 25.42 418.6 18 494.8 18 494.8h432.6s-139-21.1-147.8-75.7c-14.9-92.2 194.5-102.7 196.5-199.9.9-43.2-88.3-124.99-184.4-132.52-5.6-.44-22.7-2.71-32.2-2.48zm-163.5 40.9-32.69 10.5v122.2l35.99-10-3.3-122.7z" />
+      </svg>
+    ),
   },
   {
-    title:"Special Features",
-    points:[
+    title: "Special Features",
+    points: [
       "Crash Course Conducted",
       "Innovative Teaching Method",
       "Course Content updated frequently",
       "Separate batch for housewives",
       "Special attention to slow learners",
-      "10th, +1 and +2 English grammer tution available"
-    ]
-  }
-]
+      "10th, +1 and +2 English grammer tution available",
+    ],
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        width={40}
+        height={40}
+      >
+        <path
+          fillRule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
 
 const mapChild = (param) => {
   return param.map((item, index) => <li key={index}>{item}</li>);
 };
 
-const mapSpoken = (param) =>{
-  return param.map((item,index)=>(
+const mapSpoken = (param) => {
+  return param.map((item, index) => (
     <div
-    key={index}
-    className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 md:w-1/3 lg:w-1/4 lg:px-6 flex-grow justify-center"
-    data-wow-duration="1s"
-    styles="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;"
-  >
-    <div className="py-8 px-12 mb-12 bg-slate-300 rounded-2xl shadow-2xl transform transition duration-300 dark:shadow-primary ease-in-out hover:-translate-y-2">
-      <div className="inline-block text-gray-900 mb-4">
-        {item.svg}
+      key={index}
+      className="flex items-stretch px-4 max-w-full w-full sm:w-1/2 md:w-1/3 lg:w-1/4 lg:px-6 flex-grow justify-center"
+      data-wow-duration="1s"
+      styles="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;"
+    >
+      <div className="py-8 px-12 mb-12 bg-slate-300 rounded-2xl shadow-2xl transform transition duration-300 dark:shadow-primary ease-in-out hover:-translate-y-2">
+        <div className="inline-block text-gray-900 mb-4">{item.svg}</div>
+        <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
+          {item.title}
+        </h3>
+        <p className="text-gray-500">{mapChild(item.points)}</p>
       </div>
-      <h3 className="text-lg leading-normal mb-2 font-semibold text-black">
-        {item.title}
-      </h3>
-      <p className="text-gray-500">
-        {mapChild(item.points)}
-      </p>
     </div>
-  </div>
   ));
-}
+};
 
-const spokenList = mapSpoken(spokenArray)
+const spokenList = mapSpoken(spokenArray);
 
 const Spoken = () => {
   return (
     <>
-    <h1 className="text-center text-4xl font-semibold py-6">
-      Spoken English
-    </h1>
-    <p className="prose-h3 px-6">
-      Today is the age of globalisation it is difficult for one to
-      remainconnected with the rest of the worldwithout English Knowledge.
-      Further English Speaking prople have an edge over others in the job
-      market. And having this knowledge also gives you a passport tp break into
-      higher circles of the society. Further it is needless to say that English
-      makes one courageous and confident.
-    </p>
-    <div className="section pt-20 pb-8 md:pt-16 md:pb-0">
-      <div className="container xl:max-w-15xl mx-auto px-4">
-        <div className="flex flex-wrap flex-row -mx-4 text-center">
-          {spokenList}
+      <h1 className="text-center text-4xl font-semibold py-6">
+        Spoken English
+      </h1>
+      <p className="prose-h3 px-6">
+        Today is the age of globalisation it is difficult for one to
+        remainconnected with the rest of the worldwithout English Knowledge.
+        Further English Speaking prople have an edge over others in the job
+        market. And having this knowledge also gives you a passport tp break
+        into higher circles of the society. Further it is needless to say that
+        English makes one courageous and confident.
+      </p>
+      <div className="section pt-20 pb-8 md:pt-16 md:pb-0">
+        <div className="container xl:max-w-15xl mx-auto px-4">
+          <div className="flex flex-wrap flex-row -mx-4 text-center">
+            {spokenList}
+          </div>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 };
 
