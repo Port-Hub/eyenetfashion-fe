ecc2fcdc3a51630410e3e9ff6cfef511a1104eb8
RahulRST <srahuljayaraj@gmail.com>
2022-09-02 16:16:50 +0530

    Partial Map

--- src/components/Pages/home.jsx
+++ src/components/Pages/home.jsx
@@ -1,17 +1,72 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
-import { Suspense } from "react";
+import { Suspense,Component } from "react";
+
+class Env extends Component {
+    render() {
+        return(
+            <Environment
+                background
+                files={this.props.file}
+                path={"/home/"}
+                />
+        )
+    }
+}
+
+const hdrArray = [
+    {
+        name:"Apartment",
+        file:"Apartment.hdr"
+    },
+    {
+        name:"Forest",
+        file:"Forest.hdr"
+    },
+    {
+        name:"Sunset",
+        file:"Sunset.hdr"
+    },
+    {
+        name:"City",
+        file:"City.hdr"
+    },
+    {
+        name:"Park",
+        file:"Park.hdr"
+    },
+    {
+        name:"Dawn",
+        file:"Dawn.hdr"
+    },
+    {
+        name:"Lobby",
+        file:"Lobby.hdr"
+    },
+    {
+        name:"Night",
+        file:"Night.hdr"
+    },
+    {
+        name:"Studio",
+        file:"Studio.hdr"
+    },
+    {
+        name:"Warehouse",
+        file:"Warehouse.hdr"
+    }
+]
+
+const mapHDR = (param) => <Env file={param.file} />
+
+const hdrList = mapHDR(hdrArray)
 
 const Home = () => {
     return(
         <Canvas>
             <Suspense fallback={null} >
                 <OrbitControls />
-                <Environment 
-                    background
-                    files={"Apartment.hdr"}
-                    path={"/home/"}
-                        />
+                {hdrList}
             </Suspense>
         </Canvas>
     )
