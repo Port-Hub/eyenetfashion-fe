c34eee8d52d647258e9f69e793c1ec83061876ab
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-09-15 15:37:43 +0530

    404

---
+++ src/pages/pagenotfound.jsx
@@ -0,0 +1,13 @@
+import pgsvg from "../assets/pagenotfound/page404.svg"
+const Pagenotfound = () =>{
+
+    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+    <img
+      src={pgsvg}
+      alt=""
+      className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+    />
+  </div>
+
+}
+export default Pagenotfound
