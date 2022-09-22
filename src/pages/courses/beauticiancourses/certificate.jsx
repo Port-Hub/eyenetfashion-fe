25fcd3e8ff43cb348c800d6c8a76cdd4591577a4
RomarioKavin1 <romariokavin.25cs@licet.ac.in>
2022-09-22 23:19:34 +0530

    height issue in certification page fixed

--- src/pages/courses/beauticiancourses/certificate.jsx
+++ src/pages/courses/beauticiancourses/certificate.jsx
@@ -2,199 +2,196 @@ import { Link } from "react-router-dom";
 const Certificate = () => {
   return (
     <>
-      <div className="flex  justify-center h-screen">
+      <div className="flex  justify-center h-full">
         <div className="bg-white">
-          <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
-            <h2 className="text-2xl font-semibold">Certificate Courses</h2>
-            <p className="mt-4 mb-8 text-gray-600">
-              React Interview Questions And Answers
-            </p>
+          <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8 " >
+            <h2 className="text-2xl font-semibold pb">Certificate Courses</h2>
             <div className="space-y-4">
-              <details className="w-full rounded-lg ring-1 ring-purple-600">
+              <details className="w-full rounded-lg ring-1 primary">
                 <summary className="px-4 py-6 font-semibold">
                   Certificate in Hair Stylist
                 </summary>
                 <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600 ">
-                  <ul className="bg-white rounded-lg border border-gray-200 w-full text-gray-900 text-center">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                  <ul className="bg-white rounded-lg border border-accent w-full text-gray-900 text-center">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Curls
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Flow dry
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Nayanthara
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Christyani
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Hair attachment
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Bridal Hair style
                     </li>
                   </ul>
                 </p>
               </details>
-              <details className="w-full rounded-lg ring-1 ring-purple-600">
+              <details className="w-full rounded-lg ring-1 primary">
                 <summary className="px-4 py-6 font-semibold">
                   Certificate Course In Beautician
                 </summary>
                 <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600 ">
-                  <ul className="bg-white rounded-lg border border-gray-200 w-full text-gray-900 text-center">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                  <ul className="bg-white rounded-lg border border-accent w-full text-gray-900 text-center">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Threading (Eye-brow)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Bleach (2 types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Facial (2 types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Fruit facial
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Waxing
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Manicure
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Pedicure
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Oil Massage
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Hair cut (2 types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Henna Application
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Mehandi (Basic)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Hair style (4 types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Normal Make up
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Saree drapping (2 types)
                     </li>
                   </ul>
                 </p>
               </details>
-              <details className="w-full rounded-lg ring-1 ring-purple-600">
+              <details className="w-full rounded-lg ring-1 primary">
                 <summary className="px-4 py-6 font-semibold">
                   Certificate In Hair Care
                 </summary>
                 <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600 ">
-                  <ul className="bg-white rounded-lg border border-gray-200 w-full text-gray-900 text-center">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                  <ul className="bg-white rounded-lg border border-accent w-full text-gray-900 text-center">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Dandruff treatment
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Flow dryHair fall treatment
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Spa treatment
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Deep conditioner
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Oil Massage
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Laser Hair removal treatment
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Vates Removal treatment
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Lice Treatment
                     </li>
                   </ul>
                 </p>
               </details>
-              <details className="w-full rounded-lg ring-1 ring-purple-600">
+              <details className="w-full rounded-lg ring-1 primary">
                 <summary className="px-4 py-6 font-semibold">
                   Certificate In Makeup Stylist
                 </summary>
                 <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600 ">
-                  <ul className="bg-white rounded-lg border border-gray-200 w-full text-gray-900 text-center">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                  <ul className="bg-white rounded-lg border border-accent w-full text-gray-900 text-center">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Stage makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Bridal makeup (3 types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Metallic Makeup (Shimmering)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Reception makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Nayanthara makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Face cut makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Smoky eyes
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Party makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Saree draping (10 types)
                     </li>
                   </ul>
                 </p>
               </details>
-              <details className="w-full rounded-lg ring-1 ring-purple-600">
+              <details className="w-full rounded-lg ring-1 primary">
                 <summary className="px-4 py-6 font-semibold">
                   Certificate In Beauty Stylist
                 </summary>
 
                 <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600 ">
-                  <ul className="bg-white rounded-lg border border-gray-200 w-full text-gray-900 text-center">
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                  <ul className="bg-white rounded-lg border border-accent w-full text-gray-900 text-center">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Bridal makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Reception makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Metallic makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Smokey makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       face cut makeup
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Dolly (Normal)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Machinery Hairstyle ( two types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Bridal Hair styles ( two types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Dutch men's Hair (two types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Saree draping (four types)
                     </li>
-                    <li className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+                    <li className="px-6 py-2 border-b border-accent w-full rounded-t-lg">
                       Mehandhi (Traditional 3 types + Arabic 2 types)
                     </li>
                   </ul>
